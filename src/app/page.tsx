d24afdf4470edc16d8abc8301e14542c223d855b
aymanKhan <akhan@proficientnowtech.com>
2024-10-09 18:31:56 +0530

    NewChanges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,14 @@
-"use client";
-
-import Navbar from "./components/navbar";
-import Sidebar from "./components/asideBar";
-import FeedContainer from "./components/feedContainer";
-import FilterButtons from "./components/filterButtons";
+import Sidebar from "./components/layouts/asideBar";
+import Feed from "./components/layouts/feed";
+import NavBar from "./components/layouts/navbar";
 
 export default function Home() {
   return (
-    <div className="bg-[#0E1217] h-screen overflow-hidden">
-      {/* Navbar at the top */}
-      <Navbar />
-
-      {/* Flexbox to align Sidebar and FeedContainer */}
-      <div className="flex h-full">
-        {/* Sidebar on the left, scrollable independently */}
-        <aside className=" h-full overflow-y-scroll sticky top-0 no-scrollbar">
-          <Sidebar />
-        </aside>
-
-        {/* FeedContainer to the right of Sidebar, scrollable independently */}
-        <main className="w-[75%] p-4 h-full overflow-y-scroll no-scrollbar">
-          <FilterButtons />
-          <FeedContainer />
-        </main>
+    <div className="">
+      <NavBar />
+      <div className="w-full h-screen grid grid-cols-6 overflow-y-clip">
+        <Sidebar />
+        <Feed />
       </div>
     </div>
   );
